209e1bb33cc27aef0cce5ac3ff05139f29ceb332
dhouha <dhouha.mansour11@gmail.com>
2021-09-29 19:17:14 +0200

    http

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,7 +1,13 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Task } from '../Task';
+
+const httpOptions = {
+  headers: new  HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root',
@@ -15,8 +21,13 @@ export class TaskService {
     return this.http.get<Task[]>(this.apiUrl);
   }
 
-  deleteTask(task: Task): Observable<Task>{
+  deleteTask(task: Task): Observable<Task> {
     const url = `${this.apiUrl}/${task.id}`;
     return this.http.delete<Task>(url);
+  }
+
+  updateTaskReminder(task: Task): Observable<Task> {
+    const url = `${this.apiUrl}/${task.id}`;
+    return this.http.put<Task>(url, task, httpOptions);
   }
 }
